c041e7849e671847cd55cec1c03c563c69c1c2ad
gaurav <gauravkumar552@gmail.com>
2014-02-21 03:25:21 +0530

    Another test suite added

--- test.c
+++ test.c
@@ -7,7 +7,6 @@ START_TEST(sumtest)
 {
 ck_assert(add(3,4) == 7);
 ck_assert(add(-1,4) == 3);
-//ck_abort_msg("test failed\n");
 }
 END_TEST
 
@@ -15,7 +14,12 @@ START_TEST(subtest)
 {
 ck_assert(sub(3,4) == -1);
 ck_assert(sub(-1,4) == -5);
-//ck_abort_msg("test failed\n");
+}
+END_TEST
+
+START_TEST(mixtest)
+{
+ck_assert(add(sub(3,4),sub(-1,4)) == -6);
 }
 END_TEST
 
@@ -23,13 +27,19 @@ int main()
 {
     Suite *s1 = suite_create("Sum");
     TCase *tc1_1 = tcase_create("Core");
+    Suite *s2 = suite_create("mix");
+    TCase *tc1_2 = tcase_create("MixCore");
     int nf;
 
     suite_add_tcase(s1, tc1_1);
     tcase_add_test(tc1_1, sumtest);
     tcase_add_test(tc1_1, subtest);
 
+    suite_add_tcase(s2, tc1_2);
+    tcase_add_test(tc1_2, mixtest);
+    
     SRunner *sr = srunner_create(s1);
+    srunner_add_suite(sr,s2);
     
     srunner_run_all(sr, CK_NORMAL);
     nf = srunner_ntests_failed(sr);
