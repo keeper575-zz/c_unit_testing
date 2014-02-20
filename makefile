efd261a45c7f434031c644e0393b2fae70f4edc6
gaurav <gauravkumar552@gmail.com>
2014-02-20 07:15:43 +0530

    check framework back again.One unit test Added

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ LFLAGS=-lm
 run: lib.o main.c
 	$(CC) $(CFLAGS) -fprofile-arcs -ftest-coverage -o run lib.o main.c $(LFLAGS)
 test: lib.o test.c
-	$(CC) $(CFLAGS) -fprofile-arcs -ftest-coverage -o test lib.o test.c $(LFLAGS)
+	$(CC) $(CFLAGS) -fprofile-arcs -ftest-coverage -o test test.c lib.o $(LFLAGS) -lcheck
 lib.o: lib.c
 	$(CC) $(CFLAGS) -fprofile-arcs -ftest-coverage -c lib.c $(LFLAGS)
 clean:
--- test.c
+++ test.c
@@ -1,13 +1,32 @@
-#include<stdio.h>
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "lib.h"
-#include<stdlib.h>
-#include<assert.h>
-void check_sum()
+#include <assert.h>
+START_TEST(sumtest)
 {
-assert(add(3,4) == 7);
+ck_assert(add(3,4) == 7);
+ck_assert(add(-1,4) == 3);
+//ck_abort_msg("test failed\n");
 }
+END_TEST
+
+	
+
 int main()
 {
-check_sum();
-return 0;
+    Suite *s1 = suite_create("Sum");
+    TCase *tc1_1 = tcase_create("Core");
+    int nf;
+
+    suite_add_tcase(s1, tc1_1);
+    tcase_add_test(tc1_1, sumtest);
+
+    SRunner *sr = srunner_create(s1);
+    
+    srunner_run_all(sr, CK_NORMAL);
+    nf = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return nf == 0 ? 0 : 1;
 }
